66a671f7d930830819a7c8a9fe27df2f7a5975d1
amcc <alistairmcclymont@gmail.com>
2019-09-11 20:26:43 +0100

    changed bus no

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const BME280 = require('bme280-sensor');
 // The BME280 constructor options are optional.
 // 
 const options = {
-  i2cBusNo   : 1, // defaults to 1
+  i2cBusNo   : 0, // defaults to 1
   i2cAddress : BME280.BME280_DEFAULT_I2C_ADDRESS() // defaults to 0x77
 };
 
